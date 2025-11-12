48b8d44ced1f996642d4c5c83ee703be2d2b5d57
Angie <vett200701@gmail.com>
2025-11-11 21:58:39 -0600

    Add files via upload

---
+++ EVA3_8_SOBREECARGA/src/eva3_8_sobreecarga/EVA3_8_SOBREECARGA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_8_sobreecarga;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA3_8_SOBREECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println(100);
+        System.out.println(100.5);
+        System.out.println(true);
+        System.out.println('c');
+        System.out.println(args);
+        
+        
+    }
+    
+}
