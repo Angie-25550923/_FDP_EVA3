e6c26687e8ec8e211d4dc1d73e18475ceb293567
Angie <vett200701@gmail.com>
2025-11-14 21:52:47 -0600

    Add files via upload

--- EVA3_9_AREAS/src/eva3_9_areas/EVA3_9_AREAS.java
+++ EVA3_9_AREAS/src/eva3_9_areas/EVA3_9_AREAS.java
@@ -22,11 +22,14 @@ public class EVA3_9_AREAS {
             imprimirMenu();
             opcion = captu.nextInt();
             if (opcion==1){
-            calcularAreacirculo();
+            AreaCirculo();
+            break;
             } else if (opcion==2){
-            calcularAreaTriangulo();
+            AreaRectangulo();
+            break;
             } else if (opcion == 3) {   
-            calcularAreaTrapecio();
+            AreaTrapecio();
+            break;
             }
             
         }while(opcion !=4);
@@ -36,11 +39,11 @@ public class EVA3_9_AREAS {
         System.out.println("BIENVENIDO AL SISTEMA");
         System.out.println("Selelcciona una opción");
         System.out.println("1. Calcular area de un  Circulo");
-        System.out.println("2. Calcular area de un Triangulo");
+        System.out.println("2. Calcular area de un Rectangulo");
         System.out.println("3. Calcular area de un Trapecio");
         System.out.println("4. Salir del sistema");
     }
-    public static void calcularAreacirculo(){
+    public static void AreaCirculo(){
         Scanner captu = new Scanner(System.in);
         double radio;
         double pi = 3.1416;
@@ -48,51 +51,51 @@ public class EVA3_9_AREAS {
         System.out.println("Captura el radio:");
         radio = captu.nextDouble();
         //pendiente el calculo
-        System.out.println("El area del circulo con radio de " + radio + " es: " + calcularCirculo(radio, pi));
+        System.out.println("El area del circulo con radio de " + radio + " es: " + calcularArea(radio, pi));
     
     }
-    public static double calcularCirculo(double radio, double pi ){
-        double area = pi * radio * radio;
+    public static double calcularArea(double radio ){
+        double area = Math.PI * radio * radio;
         return area;
     
     }
-    public static void calcularAreaTriangulo() {
+    public static void AreaRectangulo(){
         Scanner captu = new Scanner(System.in);
         double base,altura;
-        System.out.println("*******CALCULAR AREA DE UN TRIANGULO********");
+        System.out.println("*******CALCULAR AREA DE UN RECTANGULO********");
         System.out.println("Capturar base:");
-        base = captu.nextInt();
+        base = captu.nextDouble();
         System.out.println("Capturar altura: ");
-        altura = captu.nextInt();
+        altura = captu.nextDouble();
         
         //pendiente el calculo
-        System.out.println("El area de un triangulo con "+ base + altura +  " es: " + calcularTriangulo(base, altura));
+        System.out.println("El area de un rectangulo con "+ base + altura +  " es: " + calcularArea(base, altura));
 
     }
     
-    public static double calcularTriangulo(double base, double altura){
-    double area =(base*altura)/2;
+    public static double calcularArea(double base, double altura){
+    double area =(base*altura);
     return area;  
     
     }
     
-    public static void calcularAreaTrapecio() {
+    public static void AreaTrapecio() {
         Scanner captu = new Scanner(System.in);
         double Base,base,altura;
         System.out.println("*******CALCULAR AREA DE UN TRAPECIO********");
         System.out.println("Capturar base mayor:");
-        Base = captu.nextInt();
+        Base = captu.nextDouble();
         System.out.println("Capturar base menor: ");
-        base= captu.nextInt();
+        base= captu.nextDouble();
         System.out.println("Capturar altura: ");
         altura= captu.nextDouble();
         
         //pendiente el calculo
-        System.out.println("El area del trapecio con bases: " + base + "," + Base + " y altura " + altura + " es: " + calcularTrapecio(Base, base, altura));
+        System.out.println("El area del trapecio con bases: " + base + "," + Base + " y altura " + altura + " es: " + calcularArea(Base, base, altura));
 
     }
     
-    public static double calcularTrapecio(double Base, double base, double altura){
+    public static double calcularArea(double Base, double base, double altura){
     double area = (Base + base)*altura % 2;
     return area;  
     
